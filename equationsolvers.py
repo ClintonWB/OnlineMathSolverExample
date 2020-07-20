17d8e4562040bce5c04a7ff871893599179d7fd3
Clinton Bradford <bradfor3@purdue.edu>
2020-07-20 00:20:25 -0400

    MVP Website

---
+++ equationsolvers.py
@@ -0,0 +1,242 @@
+from sympy import Eq, latex
+
+from sympy.parsing.sympy_parser import (parse_expr, convert_equals_signs,
+    implicit_multiplication, standard_transformations)
+from textwrap import dedent
+
+# Linear Equation Solver
+def linear_solver(sub):
+    r"""
+
+
+
+    EXAMPLES
+    --------
+
+    >>> linear_solver("")
+    False
+
+    >>> linear_solver("something abstract")
+    False
+
+    >>> linear_solver("x+1")
+    False
+
+    >>> linear_solver("x**2+1=1")
+    False
+
+    >>> print(linear_solver("x=1"))
+    Let's solve the equation:
+    \[
+        x = 1
+    \]
+    The equation is in the form $x = 1$;
+    That is, the value of $x$ is $1$.
+
+    >>> print(linear_solver("x=0"))
+    Let's solve the equation:
+    \[
+        x = 0
+    \]
+    The equation is in the form $x = 0$;
+    That is, the value of $x$ is $0$.
+
+    >>> print(linear_solver("a-1=1"))
+    Let's solve the equation:
+    \[
+        a - 1 = 1
+    \]
+    First, we subtract -1 from both sides:
+    \begin{align*}
+        (a - 1)-(-1) &= 1-(-1) \\
+        a &= 2
+    \end{align*}
+    The equation is in the form $a = 2$;
+    That is, the value of $a$ is $2$.
+
+    >>> print(linear_solver("5/3y=2"))
+    Let's solve the equation:
+    \[
+        \frac{5 y}{3} = 2
+    \]
+    We have just one term on the left:
+    The variable $y$ with coefficient $\frac{5}{3}$.
+    Divide both sides by $\frac{5}{3}$:
+    \begin{align*}
+        \frac{ \frac{5 y}{3} }{ \frac{5}{3} } &=
+        \frac{ 2 }{ \frac{5}{3} } \\
+        y &= \frac{6}{5}
+    \end{align*}
+    The equation is in the form $y = \frac{6}{5}$;
+    That is, the value of $y$ is $\frac{6}{5}$.
+
+    >>> print(linear_solver("3a-1=1"))
+    Let's solve the equation:
+    \[
+        3 a - 1 = 1
+    \]
+    First, we subtract -1 from both sides:
+    \begin{align*}
+        (3 a - 1)-(-1) &= 1-(-1) \\
+        3 a &= 2
+    \end{align*}
+    We have just one term on the left:
+    The variable $a$ with coefficient $3$.
+    Divide both sides by $3$:
+    \begin{align*}
+        \frac{ 3 a }{ 3 } &=
+        \frac{ 2 }{ 3 } \\
+        a &= \frac{2}{3}
+    \end{align*}
+    The equation is in the form $a = \frac{2}{3}$;
+    That is, the value of $a$ is $\frac{2}{3}$.
+
+    >>> print(linear_solver("a-1=1"))
+    Let's solve the equation:
+    \[
+        a - 1 = 1
+    \]
+    First, we subtract -1 from both sides:
+    \begin{align*}
+        (a - 1)-(-1) &= 1-(-1) \\
+        a &= 2
+    \end{align*}
+    The equation is in the form $a = 2$;
+    That is, the value of $a$ is $2$.
+    """
+    # Check if SymPy can parse the expression as an equation
+    try:
+        expr = parse_expr(sub,
+                   transformations=(*standard_transformations,
+                                    implicit_multiplication,
+                                    convert_equals_signs))
+    except (SyntaxError, ValueError):
+        return False
+
+    # Verify the structure of the equation
+
+    # Check if the expression is in 1 variable
+    variables = expr.free_symbols
+    if len(variables) != 1:
+        return False
+    x, = variables
+
+    # Check if it is a linear equation
+    if not isinstance(expr, Eq):
+        return False
+    if not expr.rhs.is_constant():
+        return False
+    if not expr.lhs.diff(x).is_constant():
+        return False
+
+    # Now that we know the structure of the equation,
+    # we can turn it into a worked-through solution.
+
+    explanation = dedent("""\
+    Let's solve the equation:
+    \\[
+        {expression}
+    \\]
+    """.format(expression=latex(expr)))
+    lhs = expr.lhs
+    rhs = expr.rhs
+    coeff = lhs.coeff(x)
+    left_constant = lhs - coeff*x
+
+    # Use conditional blocks to construct content that only sometimes shows up.
+    if not left_constant.is_zero:
+        new_rhs = rhs - left_constant
+        new_lhs = lhs - left_constant
+        explanation += dedent("""\
+        First, we subtract {left_constant} from both sides:
+        \\begin{{align*}}
+            ({old_lhs})-({left_constant}) &= {old_rhs}-({left_constant}) \\\\
+            {new_lhs} &= {new_rhs}
+        \\end{{align*}}
+        """.format(left_constant = left_constant,
+                   old_lhs = latex(lhs),
+                   old_rhs = latex(rhs),
+                   new_lhs = latex(new_lhs),
+                   new_rhs = latex(new_rhs),
+                   ))
+        lhs = new_lhs
+        rhs = new_rhs
+
+    if not coeff == 1:
+        new_rhs = rhs/coeff
+        new_lhs = lhs/coeff
+        explanation += dedent("""\
+        We have just one term on the left:
+        The variable ${variable}$ with coefficient ${coefficient}$.
+        Divide both sides by ${coefficient}$:
+        \\begin{{align*}}
+            \\frac{{ {old_lhs} }}{{ {coefficient} }} &=
+            \\frac{{ {old_rhs} }}{{ {coefficient} }} \\\\
+            {new_lhs} &= {new_rhs}
+        \\end{{align*}}
+        """.format(coefficient = latex(coeff),
+                   variable = latex(x),
+                   old_lhs = latex(lhs),
+                   old_rhs = latex(rhs),
+                   new_lhs = latex(new_lhs),
+                   new_rhs = latex(new_rhs),
+                   ))
+        lhs = new_lhs
+        rhs = new_rhs
+
+    explanation += dedent("""\
+        The equation is in the form ${variable} = {value}$;
+        That is, the value of ${variable}$ is ${value}$.""".format(
+        variable = latex(x),
+        value = latex(rhs)))
+
+    return explanation
+
+
+# Exponential
+def exponential_solver(sub):
+    return False
+
+
+# Logarithm
+def logarithm_solver(sub):
+    return False
+
+
+# Square Roots
+def square_root_solver(sub):
+    return False
+
+
+# Quadratic Equation Solver
+# Examples:
+#    "x**2+2x+1=0"
+#    "y**2+1=0"
+#    "z**2+3z+2=0"
+def quadratic_solver(sub):
+    return False
+
+
+# Systems of Linear Equations Solver
+# Examples:
+#     "a+2b = 1,a-b=3"
+#     "3x+2/3y=5/2,5x-y=2"
+# You can do it in only two dimensions if you want,
+# or challenge yourself to do it for more.
+def system_of_linear_equations_solver(sub):
+    return False
+
+
+# Export solvers as a list
+equation_solvers = (linear_solver,
+                    quadratic_solver,
+                    logarithm_solver,
+                    exponential_solver,
+                    square_root_solver,
+                    system_of_linear_equations_solver,
+                    )
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
---
+++ solver_website.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from flask import Flask, request, render_template
+from equationsolvers import equation_solvers as solvers
+from markdown import markdown
+
+app = Flask("Solver Website")
+
+@app.route('/')
+def solve():
+    sub = request.args.get('sub', default='')
+
+    if sub != '':
+        for solver in solvers:
+            workthrough = solver(sub)
+            if workthrough != False:
+                break
+        else:
+            workthrough = "Unable to understand expression '{}'".format(sub)
+        return render_template('index.html',workthrough = workthrough)
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
