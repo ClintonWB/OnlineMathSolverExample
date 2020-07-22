06324e9ec0a21f561f50a55c6029c44eb8ff17fc
Stian Clem <clems@purdue.edu>
2020-07-22 01:19:51 -0500

    Added Explanation to Solution

--- equationsolvers.py
+++ equationsolvers.py
@@ -186,7 +186,7 @@ def linear_solver(sub):
                    transformations=(*standard_transformations,
                                     implicit_multiplication,
                                     convert_equals_signs))
-    except (SyntaxError, ValueError):
+    except:
         return False
 
     # Verify the structure of the equation
@@ -268,7 +268,7 @@ def system_of_linear_equations_solver(sub):
                    transformations=(*standard_transformations,
                                     implicit_multiplication,
                                     convert_equals_signs)))
-    except (SyntaxError, ValueError):
+    except:
         return False
 
     # Verify the structure of the equations
@@ -305,23 +305,100 @@ def system_of_linear_equations_solver(sub):
         rhs.append(i.rhs)
         coeff.append((lhs[-1].coeff(x),lhs[-1].coeff(y)))
 
+    explanation = dedent("""\
+    Let's solve the system of equations:
+    \\[
+        \\begin{{align*}}
+            {left1}&={right1}\\\\
+            {left2}&={right2}
+        \\end{{align*}}
+    \\]
+    """.format(left1=latex(lhs[0]),left2=latex(lhs[1]),
+               right1=latex(rhs[0]),right2=latex(rhs[1])))
+
     new_coeff=[1]
     new_coeff.extend(new_coeff[-1]*i[0] for i in coeff)
     new_coeff=new_coeff[-1]
     new_lhs=[lhs[i]*new_coeff/coeff[i][0] for i in range(len(coeff))]
     new_rhs=[rhs[i]*new_coeff/coeff[i][0] for i in range(len(coeff))]
 
+    if any(i[0]!=coeff[0][0] for i in coeff):
+        explanation+=dedent("""\
+        First we multiply each equation by the coefficient of ${variable}$ in the other equations.
+        This way they will all have the same coefficient for ${variable}$.
+        """.format(variable=latex(x)))
+
+        if coeff[1][0]!=1:
+            explanation+=dedent("""\
+            \\begin{{align*}}
+                {coeff1}\\left({left1}\\right)&={coeff1}\\left({right1}\\right)\\\\
+                {new_left1}&={new_right1}
+            \\end{{align*}}
+            """.format(variable=latex(x),coeff1=latex(new_coeff/coeff[0][0]),
+                       left1=latex(lhs[0]),new_left1=latex(new_lhs[0]),
+                       right1=latex(rhs[0]),new_right1=latex(new_rhs[0])))
+
+        if coeff[0][0]!=1:
+            explanation+=dedent("""\
+            \\begin{{align*}}
+                {coeff2}\\left({left2}\\right)&={coeff2}\\left({right2}\\right)\\\\
+                {new_left2}&={new_right2}
+            \\end{{align*}}
+            """.format(variable=latex(x),coeff2=latex(new_coeff/coeff[1][0]),
+                       left2=latex(lhs[1]),new_left2=latex(new_lhs[1]),
+                       right2=latex(rhs[1]),new_right2=latex(new_rhs[1])))
+
     combined_lhs=new_lhs[0]-new_lhs[1]
     combined_rhs=new_rhs[0]-new_rhs[1]
+
+    explanation+=dedent("""\
+    If we subtract the second equation from the first, we get:
+    \\begin{{align*}}
+        {left}&={right}
+    \\end{{align*}}
+    """.format(left=latex(combined_lhs),right=latex(combined_rhs)))
+
     if combined_lhs.is_constant():
         if (combined_lhs-combined_rhs).is_zero:
-            return "infinite solutions"
-        return "no solutions"
+            explanation+=dedent("""\
+            In this case, both variables ended up cancelling out since the two
+            equations were scalar multiples of each other. Thus there are an
+            infinite number of solutions to this system of equations.
+            """)
+        else:
+            explanation+=dedent("""\
+            But these values aren't equal, so there are no solutions to this system of equations.
+            """)
+        return explanation
 
-    y_value=linear_logic(Eq(combined_lhs,combined_rhs),y)[1]
-    x_value=[linear_logic(i.subs(y,y_value),x)[1] for i in expr]
-    assert all(i==x_value[0] for i in x_value),"Deduced value for {x} should be equal for all expressions"
-    return str(x)+"="+str(x_value[0])+","+str(y)+"="+str(y_value)
+    y_explanation,y_value=linear_logic(Eq(combined_lhs,combined_rhs),y)
+    x_explanation,x_value=zip(*[linear_logic(i.subs(y,y_value),x) for i in expr])
+
+    explanation+=dedent("""\
+    Since this is now an equation in one variable, we can solve it like we would
+    any other linear equation.
+    \\begin{{align*}}
+    \\end{{align*}}
+    """.format())
+    explanation+=y_explanation
+    explanation+=dedent("""\
+    \\begin{{align*}}
+    \\end{{align*}}
+    Plugging in ${value}$ for ${variable}$ in the first equation, we can now
+    solve for ${other}$ like we would any other linear equation.
+    \\begin{{align*}}
+    \\end{{align*}}
+    """.format(value=latex(y_value),variable=latex(y),other=latex(x)))
+    explanation+=x_explanation[0]
+    explanation+=dedent("""\
+    \\begin{{align*}}
+    \\end{{align*}}
+    Thus we have found the pair of values which satisfy the two linear equations:
+    ${var1}={val1}$ and ${var2}={val2}$.
+    """.format(var1=latex(x),var2=latex(y),val1=latex(x_value[0]),val2=latex(y_value)))
+
+    assert all(i==x_value[0] for i in x_value),"Deduced value for {x} should be equal for all expressions".format(x=x)
+    return explanation
 
 # Export solvers as a list
 equation_solvers = (linear_solver,
